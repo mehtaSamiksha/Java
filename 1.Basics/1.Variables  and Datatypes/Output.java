b8fb186c6090873883722c3668d3fda15f5bd502
Samiksha <150701933+mehtaSamiksha@users.noreply.github.com>
2024-04-17 23:47:00 +0530

    Add files via upload

---
+++ 1.Basics/1.Variables  and Datatypes/Output.java
@@ -0,0 +1,20 @@
+public class Output{
+    public static void main(String[] args) {
+        System.out.println("Hello World");//the output after this will be printed in next line
+        System.out.print("Hello World");//the output after this will be printed in same line
+        System.out.print("Hello World\n");//"\n" also prints the output after this in next line
+        System.out.print("Hello World\n");
+
+        //to print pattern
+        /* 
+            *****
+            ***
+            **
+            *       
+        */
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
---
+++ 1.Basics/1.Variables  and Datatypes/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a = 5;//declaration of variable
+        int b = 10;
+        System.out.println(a);//printing variable
+        System.out.println(b);
+        String name="SRK";
+        System.out.println(name);
+
+
+        a = 50;//the value of a will change
+        System.out.println(a);
+    }
+}
